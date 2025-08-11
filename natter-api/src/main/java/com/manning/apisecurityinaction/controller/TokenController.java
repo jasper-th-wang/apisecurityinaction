36fda28520a59e07422b00faba9e0570a92cb417
Jasper Wang <jasper8777@icloud.com>
2025-08-10 17:37:22 -0700

    5.2.1 done (page 155)

--- natter-api/src/main/java/com/manning/apisecurityinaction/controller/TokenController.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/controller/TokenController.java
@@ -4,9 +4,12 @@ import java.time.temporal.ChronoUnit;
 
 import org.json.JSONObject;
 import com.manning.apisecurityinaction.token.TokenStore;
+import static spark.Spark.*;
 import spark.*;
 
 import static java.time.Instant.now;
+
+import java.time.Instant;
 
 public class TokenController {
 
@@ -17,15 +20,16 @@ public class TokenController {
     }
 
     public JSONObject login(Request request, Response response) {
-        String subject = request.attribute("subject");
-        var expiry = now().plus(10, ChronoUnit.MINUTES);
+        var tokenId = request.headers("Authorization");
+        if (tokenId == null || !tokenId.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("missing token header");
+        }
 
-        var token = new TokenStore.Token(expiry, subject);
-        var tokenId = tokenStore.create(request, token);
+        tokenId = tokenId.substring(7);
 
-        response.status(201);
-        return new JSONObject()
-                .put("token", tokenId);
+        tokenStore.revoke(request, tokenId);
+        response.status(200);
+        return new JSONObject();
     }
 
     public JSONObject logout(Request request, Response response) {
@@ -40,22 +44,60 @@ public class TokenController {
         return new JSONObject();
     }
 
+    // NOTE: cookie version
+    // public JSONObject logout(Request request, Response response) {
+    //     var tokenId = request.headers("X-CSRF-Token");
+    //     if (tokenId == null) {
+    //         throw new IllegalArgumentException("missing token header");
+    //     }
+
+    //     tokenStore.revoke(request, tokenId);
+    //     // Success response
+    //     response.status(200);
+    //     return new JSONObject();
+    // }
+
+
     public void validateToken(Request request, Response response) {
-        var tokenId = request.headers("X-CSRF-Token");
-        if (tokenId == null) return;
+        var tokenId = request.headers("Authorization");
+        if (tokenId == null || !tokenId.startsWith("Bearer ")) {
+            return;
+        }
+
+        tokenId = tokenId.substring(7);
 
         tokenStore.read(request, tokenId).ifPresent(token -> {
-            if (now().isBefore(token.expiry)) {
+            if (Instant.now().isBefore(token.expiry)) {
                 request.attribute("subject", token.username);
+
+                // apply all attribute of this token into the request context
                 token.attributes.forEach(request::attribute);
+            } else {
+                // expired -> respond with standard error response
+                response.header("WWW-Authenticate", "Bearer error=\"invalid_token\"," + "error_description=\"Expired\"");
+                halt(401);
             }
         });
-        // WARNING: CSRF attack possible
-        // tokenStore.read(request, null).ifPresent(token -> {
-        //     if (now().isBefore(token.expiry)) {
-        //         request.attribute("subject", token.username);
-        //         token.attributes.forEach(request::attribute);
-        //     }
-        // });
+
     }
+
+    // NOTE: cookie version
+    // public void validateToken(Request request, Response response) {
+    //     var tokenId = request.headers("X-CSRF-Token");
+    //     if (tokenId == null) return;
+
+    //     tokenStore.read(request, tokenId).ifPresent(token -> {
+    //         if (now().isBefore(token.expiry)) {
+    //             request.attribute("subject", token.username);
+    //             token.attributes.forEach(request::attribute);
+    //         }
+    //     });
+    //     // WARNING: CSRF attack possible
+    //     // tokenStore.read(request, null).ifPresent(token -> {
+    //     //     if (now().isBefore(token.expiry)) {
+    //     //         request.attribute("subject", token.username);
+    //     //         token.attributes.forEach(request::attribute);
+    //     //     }
+    //     // });
+    // }
 }
